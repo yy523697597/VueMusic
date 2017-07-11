317351bc14df5e5bbc090f6083b4d1628b76eaec
yy523697597 <523697597@qq.com>
2017-07-11 17:31:58 +0800

    V1.0 完成首页骨架

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        排行榜
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'common/stylus/index.styl'
+import fastclick from 'fastclick'
+Vue.config.productionTip = false
+fastclick.attach(document.body)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
