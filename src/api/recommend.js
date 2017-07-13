62dfe285742f85d2092118c6938e467b2889f821
yy523697597 <523697597@qq.com>
2017-07-13 17:18:08 +0800

    V1.2 初步完成轮播组件

---
+++ src/api/recommend.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2017/7/12.
+ */
+import jsonp from 'common/js/jsonp'
+import {commonParams, options} from 'config'
+export function getRecommand () {
+  const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
+  const data = Object.assign({}, commonParams, {
+    platform: 'h5',
+    uin: 0,
+    needNewCode: 1
+  })
+  return jsonp(url, data, options)
+}
